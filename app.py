a833617d6be75831a9e2c266e0f25df33b90215c
Aleš <foxxcz@gmail.com>
2022-06-27 12:21:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     with open('data.json') as data_file:        
-    data = json.load(data_file)
-    for item in data:
-        print item['id']
-    return data
+        data = json.load(data_file)
+        for item in data:
+            print item['id']
+        return data
 
 def create_app():
    return app
